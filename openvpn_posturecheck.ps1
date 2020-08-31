3acd9323ae729dfb6bcf03cd08aaa3eda6f2af0f
erichester76 <eric.hester@gmail.com>
2020-08-31 19:03:05 -0400

    Initial Commit

---
+++ openvpn_posturecheck.ps1
@@ -0,0 +1,72 @@
+﻿# script originally by chentiangemalc
+# http://chentiangemalc.wordpress.com
+# modified by eric hester (eric.hester@umbrella.tech) to function as openvpn posture check for windows
+# 
+# if you want to modify what services are checked, simply remove the items from the _ALL list below
+# otherwise all services must be "good" 
+#
+
+# Define our C# functions to extract info from Windows Security Center (WSC) via Windows API so we can call from PowerShell
+$wscDefinition = @"
+        // WSC_SECURITY_PROVIDER as defined in Wscapi.h or http://msdn.microsoft.com/en-us/library/bb432509(v=vs.85).aspx
+	[Flags]
+        public enum WSC_SECURITY_PROVIDER : int
+        {
+            WSC_SECURITY_PROVIDER_FIREWALL = 1,				// The aggregation of all firewalls for this computer.
+            WSC_SECURITY_PROVIDER_AUTOUPDATE_SETTINGS = 2,		// The automatic update settings for this computer.
+            WSC_SECURITY_PROVIDER_ANTIVIRUS = 4,			// The aggregation of all antivirus products for this computer.
+            WSC_SECURITY_PROVIDER_ANTISPYWARE = 8,			// The aggregation of all anti-spyware products for this computer.
+            WSC_SECURITY_PROVIDER_INTERNET_SETTINGS = 16,		// The settings that restrict the access of web sites in each of the Internet zones for this computer.
+            WSC_SECURITY_PROVIDER_USER_ACCOUNT_CONTROL = 32,		// The User Account Control (UAC) settings for this computer.
+            WSC_SECURITY_PROVIDER_SERVICE = 64,				// The running state of the WSC service on this computer.
+            WSC_SECURITY_PROVIDER_NONE = 0,				// None of the items that WSC monitors.
+			
+	    // All of the items that the WSC monitors.
+            WSC_SECURITY_PROVIDER_ALL = WSC_SECURITY_PROVIDER_FIREWALL | WSC_SECURITY_PROVIDER_AUTOUPDATE_SETTINGS | WSC_SECURITY_PROVIDER_ANTIVIRUS |
+            WSC_SECURITY_PROVIDER_ANTISPYWARE | WSC_SECURITY_PROVIDER_INTERNET_SETTINGS | WSC_SECURITY_PROVIDER_USER_ACCOUNT_CONTROL |
+            WSC_SECURITY_PROVIDER_SERVICE | WSC_SECURITY_PROVIDER_NONE
+        }
+
+        [Flags]
+        public enum WSC_SECURITY_PROVIDER_HEALTH : int
+        {
+            WSC_SECURITY_PROVIDER_HEALTH_GOOD, 			// The status of the security provider category is good and does not need user attention.
+            WSC_SECURITY_PROVIDER_HEALTH_NOTMONITORED,		// The status of the security provider category is not monitored by WSC. 
+            WSC_SECURITY_PROVIDER_HEALTH_POOR, 			// The status of the security provider category is poor and the computer may be at risk.
+            WSC_SECURITY_PROVIDER_HEALTH_SNOOZE, 		// The security provider category is in snooze state. Snooze indicates that WSC is not actively protecting the computer.
+            WSC_SECURITY_PROVIDER_HEALTH_UNKNOWN
+        }
+
+	// as defined in http://msdn.microsoft.com/en-us/library/bb432506(v=vs.85).aspx
+        [DllImport("wscapi.dll")]
+        private static extern int WscGetSecurityProviderHealth(int inValue, ref int outValue);
+
+	// code to call our interop function and return the relevant result based on what input value we provide
+        public static WSC_SECURITY_PROVIDER_HEALTH GetSecurityProviderHealth(WSC_SECURITY_PROVIDER inputValue)
+        {
+            int inValue = (int)inputValue;
+            int outValue = -1;
+
+            int result = WscGetSecurityProviderHealth(inValue, ref outValue);
+
+            foreach (WSC_SECURITY_PROVIDER_HEALTH wsph in Enum.GetValues(typeof(WSC_SECURITY_PROVIDER_HEALTH)))
+                if ((int)wsph == outValue) return wsph;
+
+            return WSC_SECURITY_PROVIDER_HEALTH.WSC_SECURITY_PROVIDER_HEALTH_UNKNOWN;
+        }
+"@
+
+$wscType=Add-Type -memberDefinition $wscDefinition -name "wscType" -UsingNamespace "System.Reflection","System.Diagnostics" -PassThru
+
+if ($wscType[0]::GetSecurityProviderHealth($wscType[1]::WSC_SECURITY_PROVIDER_ALL) -ne $wscType[2]::WSC_SECURITY_PROVIDER_HEALTH_GOOD)
+{
+//disconnect all tunnels
+openvpn-gui.exe --command disconnect_all
+//display error dialog 
+Add-Type -AssemblyName PresentationCore,PresentationFramework
+$ButtonType = [System.Windows.MessageBoxButton]::OK
+$MessageIcon = [System.Windows.MessageBoxImage]::Error
+$MessageBody = "Your computer does not meet the security requirements to connect to this network. Please see Windows Security Center for more information. Correct all errors and try again"
+$MessageTitle = "Compliance Error"
+[System.Windows.MessageBox]::Show($MessageBody,$MessageTitle,$ButtonType,$MessageIcon)
+}
